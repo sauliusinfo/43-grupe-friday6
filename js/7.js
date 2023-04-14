5cfc35715280e554bacdb517b23f0bf8925982a4
Saulius <saulius.j@icloud.com>
2023-04-14 17:46:07 +0300

    task 7

---
+++ js/7.js
@@ -0,0 +1,16 @@
+console.clear();
+console.log(`
+7. Pirmos lietuviškos litų monetos išėjo 1925 metais. Tai buvo 5, 2 ir 1 lito vertės sidabrinės monetos. 
+Duotą pinigų sumą n litais pakeiskite nurodytomis monetomis, panaudojant kuo mažiau monetų. 
+Atsakymo galimas pvz: „Norint gauti n sumą litais, mums reikės n1 monetų po 5 lt, n2 monetų po 2 litus ir n3 monetų po 1 litą. 
+Viso monetų N skaičius“.
+`);
+
+const n1=5, n2=2, n3=1; // monetu nominalai;
+let n=0; // suma kueria reikia iskeisti;
+let N=0; // monetu kiekis;
+
+function keitykla() {
+
+}
+console.log('bla');
